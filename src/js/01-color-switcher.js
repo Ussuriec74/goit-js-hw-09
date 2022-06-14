732a255e6b2c1d64d13ddca6a551edc756d9b8fd
Valeriy Semko <valsemko74@gmail.com>
2022-06-14 15:40:20 +0300

    start and task-01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const startBtnRef = document.querySelector("[data-start]");
+const stopBtnRef = document.querySelector("[data-stop]");
+const bodyRef = document.querySelector("body");
+let intervalId
+
+startBtnRef.addEventListener("click", onClickStartBtn);
+stopBtnRef.addEventListener("click", onClickStopBtn);
+
+function onClickStartBtn() {
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    startBtnRef.removeEventListener("click", onClickStartBtn);
+  }, 1000)
+}
+
+function onClickStopBtn() {
+  clearInterval(intervalId);
+  startBtnRef.addEventListener("click", onClickStartBtn);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
